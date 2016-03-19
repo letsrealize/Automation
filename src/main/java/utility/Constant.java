204ed33121b6bd3fc4e54612ddfb49cae3b0c53a
Kiran Gautam <ukiras@gmail.com>
2016-03-18 17:39:03 -0700

    Something added

--- src/main/java/utility/Constant.java
+++ src/main/java/utility/Constant.java
@@ -18,39 +18,60 @@ public class Constant {
 		}
 	}
 
-	public static class envDetail {
-		public static class QA {
-			public static final String userName = "kgautam_con";
-			public static final String password = "kgGR2017";
+	public static class getDBDetail {
+		public static final String connectionDetailQA = "jdbc:oracle:thin:@ebsqa-scan.grc-network.net:1521/s_appdbtst";
+		public static final String userQA = "setup";
+		public static final String passQA = "APPTwe25yZ";
+		public static final String connectionDetailINT = "jdbc:oracle:thin:@ebsqa-scan.grc-network.net:1521/appdbint";
+		public static final String userINT = "setup";
+		public static final String passINT = "APPIl94ma6";
+		public static final String connectionDetailUAT = "jdbc:oracle:thin:@ebsqa-scan.grc-network.net:1521/s_appdbuat";
+		public static final String userUAT = "setup";
+		public static final String passUAT = "APPUI3erty";
 
-			public static class database {
-				public static final String connectionDetail = "jdbc:oracle:thin:@ebsqa-scan.grc-network.net:1521/s_appdbtst";
-				public static final String user = "setup";
-				public static final String pass = "APPTwe25yZ";
+		public static String getDBUserName(String env) {
+			String user = null;
+			switch (env) {
+			case "QA":
+				user = userQA;
+				break;
+			case "INT":
+				user = userINT;
+			case "UAT":
+				user = userUAT;
+				break;
 			}
+			return user;
 		}
 
-		public static class INT {
-			public static final String userName = "kgautam_con";
-			public static final String password = "kgGR2017";
-
-			public static class database {
-				public static final String connectionDetail = "jdbc:oracle:thin:@ebsqa-scan.grc-network.net:1521/appdbint";
-				public static final String user = "setup";
-				public static final String pass = "APPIl94ma6";
+		public static String getDBPass(String env) {
+			String pass = null;
+			switch (env) {
+			case "QA":
+				pass = passQA;
+				break;
+			case "INT":
+				pass = passINT;
+			case "UAT":
+				pass = passUAT;
+				break;
 			}
+			return pass;
 		}
 
-		public static class UAT {
-			public static final String userName = "kgautam_con";
-			public static final String password = "kgGR2017";
-
-			public static class database {
-				public static final String connectionDetail = "jdbc:oracle:thin:@ebsqa-scan.grc-network.net:1521/s_appdbuat";
-
-				public static final String user = "setup";
-				public static final String pass = "APPUI3erty";
+		public static String getDBconnectionDetail(String env) {
+			String connectionDetail = null;
+			switch (env) {
+			case "QA":
+				connectionDetail = connectionDetailQA;
+				break;
+			case "INT":
+				connectionDetail = connectionDetailINT;
+			case "UAT":
+				connectionDetail = connectionDetailUAT;
+				break;
 			}
+			return connectionDetail;
 		}
 	}
 
@@ -58,24 +79,27 @@ public class Constant {
 	public static final String File_TestData = "testData.xlsx";
 	public static final String Path_ScreenShot = System.getProperty("user.dir") + "//src//main//java//screenShots//";
 
+	public static final String userName = "kgautam_con";
+	public static final String password = "kgGR2017";
+
 	public static final int Col_TestCaseName = 0;
 	public static final int Col_Env = 1;
 	public static final int Col_Browser = 2;
 	public static final int Col_UserName = 3;
 	public static final int Col_Password = 4;
 	public static final int Col_ProductLine = 5;
+
 	// Test Data Sheet Columns
-	public static class shippingTableData
-	{
-	public static final int Col_Description = 6;
-	public static final int Col_Currency = 7;
-	public static final int Col_Intent = 8;
-	public static final int Col_StartDate = 9;	
-	public static final int Col_EndDate = 10;	
-	public static final int Col_UpperBase = 11;	
-	public static final int Col_Standard = 12;	
-	public static final int Col_Rush = 13;	
-	public static final int Col_2Day = 14;
-	public static final int Col_SameDay = 15;	
+	public static class shippingTableData {
+		public static final int Col_Description = 6;
+		public static final int Col_Currency = 7;
+		public static final int Col_Intent = 8;
+		public static final int Col_StartDate = 9;
+		public static final int Col_EndDate = 10;
+		public static final int Col_UpperBase = 11;
+		public static final int Col_Standard = 12;
+		public static final int Col_Rush = 13;
+		public static final int Col_2Day = 14;
+		public static final int Col_SameDay = 15;
 	}
 }
--- src/main/java/utility/SqlQueries.java
+++ src/main/java/utility/SqlQueries.java
@@ -1,5 +1,59 @@
 package utility;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
 public class SqlQueries {
+
+	public static String[] getPPIDsForPriceTable(String num, String productLine, String env) {
+		int x = Integer.parseInt(num);
+		String[] prod = new String[x];
+		try {
+			int i = 0;
+			Connection con = DriverManager.getConnection(Constant.getDBDetail.getDBconnectionDetail(env),
+					Constant.getDBDetail.getDBUserName(env), Constant.getDBDetail.getDBPass(env));
+			String sql = ("SELECT PPID FROM (SELECT PPID FROM products where PRODUCT_STATUS = 'A' and Product_line_code = '"
+					+ productLine + "' ORDER BY DBMS_RANDOM.RANDOM) WHERE  rownum <=" + num);
+			CallableStatement callStmt = con.prepareCall(sql);
+			callStmt.execute();
+			ResultSet rst = callStmt.getResultSet();
+			while (rst.next()) {
+				String product = rst.getString("PPID");
+				prod[i] = product;
+				i++;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return prod;
+
+	}
+
+	public static String[] getPPPIDsForOffer(String num, String productLine, String env) {
+		int x = Integer.parseInt(num);
+		String[] prod = new String[x];
+		try {
+			int i = 0;
+			Connection con = DriverManager.getConnection(Constant.getDBDetail.getDBconnectionDetail(env),
+					Constant.getDBDetail.getDBUserName(env), Constant.getDBDetail.getDBPass(env));
+			String sql = ("SELECT ITEM_CODE FROM (SELECT ITEM_CODE FROM s_items where ITEM_STATUS = 'Active' and RECORD_TYPE = 'PPID' and Product_line_code = '"
+					+ productLine + "' ORDER BY DBMS_RANDOM.RANDOM) WHERE  rownum <=" + num);
+			CallableStatement callStmt = con.prepareCall(sql);
+			callStmt.execute();
+			ResultSet rst = callStmt.getResultSet();
+			while (rst.next()) {
+				String product = rst.getString("ITEM_CODE");
+				prod[i] = product;
+				i++;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return prod;
+	}
 
 }
